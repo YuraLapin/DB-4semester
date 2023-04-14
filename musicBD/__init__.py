15b8d1e8059f6abc7fc64162cad620c358580292
Юрий Лапин <8chewbacca@gmail.com>
2023-04-14 19:19:37 +0500

    started changing to mysql

--- musicBD/__init__.py
+++ musicBD/__init__.py
@@ -1,32 +1,27 @@
-import sqlite3
-from flask import Flask, request, render_template, g
+from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
+import pymysql
 
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '19283sieben'
+app.config['MYSQL_DATABASE_DB'] = 'music_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
+connection = mysql.connect()
+cursor = connection.cursor()
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-DATABASE = 'musicBD/db/database.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
 @app.route('/make_request', methods = ['POST'])
 def make_request():
-    db = get_db()
     sql_request = request.form.get('request_text')
-    request_result = db.execute(sql_request)
+    cursor.execute(sql_request)
+    request_result = cursor.fetchall()
     result_headers = [description[0] for description in request_result.description]
     resut_headers_tuple = tuple(result_headers)
     fetched_result = request_result.fetchall()
